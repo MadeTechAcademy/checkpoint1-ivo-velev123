220bd5c6b721916780803cc83b8f7542fb1a790e
Ivo-velev <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>
2025-07-03 11:03:22 +0100

    made a folder for html comparison files

--- test_themes.py
+++ test_themes.py
@@ -1,8 +1,8 @@
-from themes import duties_list, show_duties_in_html
+from themes import duties_list
 
 def test_duties_list():
     assert len(duties_list) == 13
 
 def test_output_to_html():
-    with open("dutiesComparison.html", "r") as f, open("duties.html", "r") as j:
+    with open("comparison_files/dutiesComparison.html", "r") as f, open("duties.html", "r") as j:
         assert f.readlines() == j.readlines()
