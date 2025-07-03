15214cc02d2f9b55d8be5f7809994c181cc1b3d7
Ivo-velev <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>
2025-07-03 11:12:24 +0100

    made another fix to test and duties list for part 2

--- test_themes.py
+++ test_themes.py
@@ -1,7 +1,7 @@
 from themes import duties_list
 
 def test_duties_list():
-    assert len(duties_list) == 14
+    assert len(duties_list) == 13
 
 def test_output_to_html():
     with open("comparison_files/dutiesComparison.html", "r") as f, open("duties.html", "r") as j:
