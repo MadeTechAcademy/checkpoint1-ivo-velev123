148d230c8a2344c79e1c9753678a0b6f64a56583
Ivo-velev <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>
2025-07-03 11:24:39 +0100

    made more fixes to tests and html comparison files

--- test_themes.py
+++ test_themes.py
@@ -4,8 +4,8 @@ def test_duties_list():
     assert len(duties_list) == 13
 
 def test_output_to_html():
-    with open("comparison_files/dutiesComparison.html", "r") as comparison_file, open("duties.html", "r") as output_file:
+    with open("comparison_files/dutiesComparison.html", "r") as comparison_file, open("output_files/duties.html", "r") as output_file:
         assert comparison_file.readlines() == output_file.readlines()
 
-    with open("comparison_files/bootcampComparison.html", "r") as comparison_file, open("bootCampDuties.html", "r") as output_file:
+    with open("comparison_files/bootcampComparison.html", "r") as comparison_file, open("output_files/bootCampDuties.html", "r") as output_file:
         assert comparison_file.readlines() == output_file.readlines()
