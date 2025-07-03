c38e3405826753079072b93f81541cd64552a532
Ivo-velev <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>
2025-07-03 11:00:51 +0100

    made minor change to a test

--- test_themes.py
+++ test_themes.py
@@ -4,6 +4,5 @@ def test_duties_list():
     assert len(duties_list) == 13
 
 def test_output_to_html():
-    show_duties_in_html()
     with open("dutiesComparison.html", "r") as f, open("duties.html", "r") as j:
         assert f.readlines() == j.readlines()
