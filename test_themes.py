074cf221d0cfda740abbf0016f80b2b02126ebd6
Ivo-velev <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>
2025-07-03 11:11:19 +0100

    made a new test and fixed and old test and function from part 2

--- test_themes.py
+++ test_themes.py
@@ -1,8 +1,11 @@
 from themes import duties_list
 
 def test_duties_list():
-    assert len(duties_list) == 13
+    assert len(duties_list) == 14
 
 def test_output_to_html():
     with open("comparison_files/dutiesComparison.html", "r") as f, open("duties.html", "r") as j:
+        assert f.readlines() == j.readlines()
+
+    with open("comparison_files/bootcampComparison.html", "r") as f, open("bootCampDuties.html", "r") as j:
         assert f.readlines() == j.readlines()
